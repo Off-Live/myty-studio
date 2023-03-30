f16335104206886fa5f9b61b793c43a4b389f997
Febraury24-Lee <February24-Lee@gmail.com>
2023-03-30 16:22:37 +0700

    feat: date format change

--- src/@types/assetCollection.ts
+++ src/@types/assetCollection.ts
@@ -1,18 +1,19 @@
 import { ethereumAddress } from './wallet';
 
 export type SupportAddress = ethereumAddress;
-export type SupportedTokenType = 'All' | 'Custom';
+// export type SupportedTokenType = 'All' | 'Custom';
 export type TokenID = string;
 export type Date = string;
-export type AvatarCompatibility = '2D' | 'FaceAR' | 'VR';
+export type AvatarCompatibility = '2D' | 'Head only' | 'Full body';
 
 export type AssetCollectionItems = {
   avatarName: string;
   creator: SupportAddress;
+  collectionAdress: SupportAddress;
   version: string;
   linkedNFT: string;
-  supportedTokenType: SupportedTokenType;
-  supportedTokenId?: TokenID[];
+  // supportedTokenType: SupportedTokenType;
+  supportedTokenId: TokenID[];
   createdDate: Date;
   compatiblity: AvatarCompatibility[];
   format: string;
