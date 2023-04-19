1785043fb5e10b387a42a629d0077b38181cb155
Febraury24-Lee <February24-Lee@gmail.com>
2023-04-19 17:42:10 +0900

    chore: add some dependency configs

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -6,11 +6,6 @@ import localStorageAvailable from '../utils/localStorageAvailable';
 //
 import { defaultLang } from './config-lang';
 //
-import enLocales from './langs/en';
-import frLocales from './langs/fr';
-import viLocales from './langs/vi';
-import cnLocales from './langs/cn';
-import arLocales from './langs/ar';
 
 // ----------------------------------------------------------------------
 
@@ -26,13 +21,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      en: { translations: enLocales },
-      fr: { translations: frLocales },
-      vi: { translations: viLocales },
-      cn: { translations: cnLocales },
-      ar: { translations: arLocales },
-    },
+    resources: {},
     lng,
     fallbackLng: defaultLang.value,
     debug: false,
