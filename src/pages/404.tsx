9a81e46a3c7317b6e3fde73e9bcbff5624cd2704
Febraury24-Lee <February24-Lee@gmail.com>
2023-04-21 00:49:29 +0900

    feat: modify 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,20 @@
-import { m } from 'framer-motion';
 // next
 import Head from 'next/head';
 import NextLink from 'next/link';
+import Image from 'next/image';
 // @mui
-import { Button, Typography } from '@mui/material';
-// layouts
-import CompactLayout from '../layouts/compact';
+import { Box, Button, Container, Stack, Typography, styled } from '@mui/material';
 // components
-import { MotionContainer, varBounce } from '../components/animate';
-// assets
-import { PageNotFoundIllustration } from '../assets/illustrations';
+import MainLayout from 'src/layouts/main/MainLayout';
+import gif404 from 'public/assets/gif/404-repeat.gif';
+import Iconify from 'src/components/iconify/Iconify';
 
 // ----------------------------------------------------------------------
 
-Page404.getLayout = (page: React.ReactElement) => <CompactLayout>{page}</CompactLayout>;
+Page404.getLayout = (page: React.ReactElement) => <MainLayout>{page}</MainLayout>;
+const GifImage = styled(Image)`
+  height: 347;
+`;
 
 // ----------------------------------------------------------------------
 
@@ -21,36 +22,34 @@ export default function Page404() {
   return (
     <>
       <Head>
-        <title> 404 Page Not Found | Minimal UI</title>
+        <title> 404 Page Not Found | MYTY-STUDIO</title>
       </Head>
 
-      <MotionContainer>
-        <m.div variants={varBounce().in}>
-          <Typography variant="h3" paragraph>
-            Sorry, page not found!
-          </Typography>
-        </m.div>
-
-        <m.div variants={varBounce().in}>
-          <Typography sx={{ color: 'text.secondary' }}>
-            Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be
-            sure to check your spelling.
-          </Typography>
-        </m.div>
-
-        <m.div variants={varBounce().in}>
-          <PageNotFoundIllustration
-            sx={{
-              height: 260,
-              my: { xs: 5, sm: 10 },
-            }}
-          />
-        </m.div>
-
-        <Button component={NextLink} href="/" size="large" variant="contained">
-          Go to Home
-        </Button>
-      </MotionContainer>
+      <Container>
+        <Stack gap={0} sx={{ alignItems: 'center' }}>
+          <Stack sx={{ maxWidth: 582, alignItems: 'center', pt: 7.5 }} gap={1}>
+            <GifImage src={gif404} alt="404" />
+            <Typography variant="h2" sx={{ color: 'text.secondary' }}>
+              404
+            </Typography>
+            <Typography variant="h3" sx={{ color: 'text.primary' }}>
+              Bad Request
+            </Typography>
+          </Stack>
+          <Box sx={{ pt: 5 }}>
+            <Button
+              component={NextLink}
+              href="/"
+              size="large"
+              variant="contained"
+              sx={{ width: 172, height: 48 }}
+              startIcon={<Iconify icon="ic:baseline-arrow-back" sx={{ width: 16, height: 16 }} />}
+            >
+              Back to Home
+            </Button>
+          </Box>
+        </Stack>
+      </Container>
     </>
   );
 }
