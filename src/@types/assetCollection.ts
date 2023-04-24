c243c53d557795daae0be93a9350798d56eb7875
Febraury24-Lee <February24-Lee@gmail.com>
2023-04-24 09:51:44 +0900

    add: avatar type

--- src/@types/assetCollection.ts
+++ src/@types/assetCollection.ts
@@ -2,9 +2,10 @@ import { ethereumAddress } from './wallet';
 
 export type SupportAddress = ethereumAddress;
 export type SupportedChain = 'Ethereum' | 'Polygon';
+export type AvatarType = '2D' | '3D';
+export type AvatarCompatibility = '2D' | 'Head' | 'Body';
 export type TokenID = string;
 export type Date = string;
-export type AvatarCompatibility = '2D' | 'Head' | 'Body';
 
 export type AssetCollectionItems = {
   chain: SupportedChain;
