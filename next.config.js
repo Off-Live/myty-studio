a47bac86134f641ff63e9ed93fa789813cf337c1
Febraury24-Lee <February24-Lee@gmail.com>
2023-05-02 23:12:51 +0900

    chore & mock-up data

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   trailingSlash: true,
   env: {
     // HOST
-    HOST_API_KEY: 'https://api-dev-minimal-v4.vercel.app',
+    HOST_API_KEY: 'http://localhost:8081',
     // MAPBOX
     MAPBOX_API: '',
     // FIREBASE
--- src/_mock/assets/my-collections-dashboard-items.ts
+++ src/_mock/assets/my-collections-dashboard-items.ts
@@ -3,6 +3,48 @@ import { AssetCollectionItems } from 'src/@types/assetCollection';
 export const myCollectionsDashboardItems: AssetCollectionItems[] = [
   {
     chain: 'Ethereum',
+    avatarName: 'MOCKUP-Board Ape Yacht Club',
+    creator: 'offlive.eth',
+    collectionAdress: '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D',
+    version: 'v1.2.0',
+    linkedNFT: 'BoardApeYachtClub',
+    supportedTokenId: ['0', '1', '2', '4', '5', '6', '7', '8', '9', '10', '11'],
+    createdDate: '07 Aug 2022',
+    compatiblity: ['Head', 'Body', '2D'],
+    format: 'Kit v1.1.2',
+    viewerURL:
+      'https://avatar-stag.myty.space/registry?version=14&nftCollectionAddress=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D',
+  },
+  {
+    chain: 'Ethereum',
+    avatarName: 'MOCKUP-Doodles',
+    creator: 'offlive.eth',
+    collectionAdress: '0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e',
+    version: 'v1.2.0',
+    linkedNFT: 'Doodles',
+    supportedTokenId: ['0', '1', '2', '4', '5', '6', '7', '8', '9', '10', '11'],
+    createdDate: '07 Aug 2022',
+    compatiblity: ['Head', 'Body', '2D'],
+    format: 'Kit v1.1.2',
+    viewerURL:
+      'https://avatar-stag.myty.space/registry?version=8&nftCollectionAddress=0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e',
+  },
+  {
+    chain: 'Ethereum',
+    avatarName: 'MOCKUP-TripleS',
+    creator: 'offlive.eth',
+    collectionAdress: '0x1a2f71468f656e97c2f86541e57189f59951efe7',
+    version: 'v1.2.0',
+    linkedNFT: 'TripleS',
+    supportedTokenId: ['0'],
+    createdDate: '07 Aug 2022',
+    compatiblity: ['Head', 'Body', '2D'],
+    format: 'Kit v1.1.2',
+    viewerURL:
+      'https://viewer-stag.myty.space/registry?version=16&nftCollectionAddress=0x1a2f71468f656e97c2f86541e57189f59951efe7',
+  },
+  {
+    chain: 'Ethereum',
     avatarName: 'BAYCaaaaaaaaaaaaaaaaaa',
     creator: 'offlive.eth',
     collectionAdress: '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D',
